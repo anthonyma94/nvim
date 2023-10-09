ce1916e2e0b2306e205439dc3c92f3266b5bdafb
Anthony Ma <github@anthonyma.ca>
2023-10-08 23:21:42 -0400

    update

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-cd ~
-mkdir tmp
-cd tmp
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar -xvzf nvim-linux64.tar.gz
-sudo mv nvim-linux64 /opt/nvim
-sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
-cd ../
-rm -rf tmp
+if ! command -v nvim; then
+	mkdir tmp
+	cd tmp
+	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+	tar -xvzf nvim-linux64.tar.gz
+	sudo mv nvim-linux64 /opt/nvim
+	sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
+	cd ../
+	rm -rf tmp
 
-sudo apt install -y xclip # needed to make clipboard work
+	sudo apt install -y xclip # needed to make clipboard work
+else
+	echo "nvim installed. Skipping..."
+fi
