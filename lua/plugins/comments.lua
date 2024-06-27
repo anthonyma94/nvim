f99e51eab15cd8ff7989de6e48f56f96a72e43be
Anthony Ma <github@anthonyma.ca>
2024-06-27 15:09:08 -0400

    added nix lsp

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -2,6 +2,10 @@ return {
   {
     "echasnovski/mini.comment",
     opts = {},
+    config = function()
+      require("mini.comment").setup()
+      vim.api.nvim_command("autocmd FileType nix setlocal commentstring=#\\ %s")
+    end,
     lazy = false,
   },
 }
