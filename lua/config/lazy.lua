34e2c5049b5fc19d31a4d6a13de948050ab24ea9
Anthony Ma <github@anthonyma.ca>
2026-07-26 14:42:11 -0400

    add clipboard provider for remote terminal

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,39 +1,52 @@
 -- Bootstrap lazy.nvim (auto-install the plugin manager if it doesn't exist)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  -- Clone lazy.nvim if not found
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    -- Clone lazy.nvim if not found
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath) -- Add lazy.nvim to runtime path
 
 -- Set leader keys before lazy loads
-vim.g.mapleader = " " -- Space as leader key
+vim.g.mapleader = " "       -- Space as leader key
 vim.g.maplocalleader = "\\" -- Backslash as local leader
 
+-- Set OSC52 for copy-paste via Windows terminal
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
+
 -- Load core configuration
-require("config.options") -- Editor options (line numbers, indentation, etc.)
-require("config.keymaps") -- Key mappings
+require("config.options")  -- Editor options (line numbers, indentation, etc.)
+require("config.keymaps")  -- Key mappings
 require("config.autocmds") -- Auto commands
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- Import all plugins from lua/plugins/
-    { import = "plugins" },
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- Auto-check for plugin updates
-  performance = {
-    rtp = {
-      -- Disable built-in plugins we don't need (faster startup)
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+    spec = {
+        -- Import all plugins from lua/plugins/
+        { import = "plugins" },
     },
-  },
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = { enabled = true }, -- Auto-check for plugin updates
+    performance = {
+        rtp = {
+            -- Disable built-in plugins we don't need (faster startup)
+            disabled_plugins = {
+                "gzip",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
 })
